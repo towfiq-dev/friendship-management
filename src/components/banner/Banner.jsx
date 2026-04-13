abd05f6b0bebca72faf070e726c2a9090547d673
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-13 19:33:25 +0600

    added banner design

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FaPlus } from 'react-icons/fa';
 
 const Banner = () => {
   return (
     <div className='text-center'>
-      Banner
+      <h1 className='text-4xl font-semibold'>Friends to keep close in your life</h1>
+      <p>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+      
+      <button className='btn bg-[#244D3F]'>
+        <FaPlus />
+        Add a Friend
+      </button>
     </div>
   );
 };
