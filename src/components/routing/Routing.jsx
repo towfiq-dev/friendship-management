6494bab58a0aa30d265184ca03e3be770b89f01e
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-14 12:57:25 +0600

    state-section functionality added

--- src/components/routing/Routing.jsx
+++ src/components/routing/Routing.jsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPages from "../errorPages/ErrorPages";
 import MainLayouts from "../layouts/MainLayouts";
-import HomePages from "../homePages/HomePages";
-import Home from "../allNavButton/home/Home";
+import HomePages from "../homePages/HomePages"
 import Timeline from "../allNavButton/timeline/Timeline";
 import Stats from "../allNavButton/stats/Stats";
 import FriendsDetails from "../all friends details/friendsDetails";
@@ -15,10 +14,6 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: HomePages,
-      },
-      {
-        path: "/home",
-        Component: Home,
       },
       {
         path: "/timeline",
--- src/components/stateSection/State.jsx
+++ src/components/stateSection/State.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-
+import React, { use } from "react";
+const totalFetch = fetch('/friends.json').then(res=> res.json())
 const State = () => {
+  const result = use(totalFetch)
+  const onTrackCount = result.filter(item => item.status === 'on track').length;
+  const needAttentionCount = result.filter(item => item.status === 'overdue').length;
   return (
     <div className="grid grid-cols-2 p-4 md:grid-cols-4 gap-5 mt-10 mb-7 max-w-300 mx-auto border-b border-b-gray-500 pb-6">
       <div className="text-center space-y-3 p-5 rounded-xl shadow-md">
-        <h1 className="text-2xl md:text-3xl font-semibold">10</h1>
+        <h1 className="text-2xl md:text-3xl font-semibold">{result.length}</h1>
         <p className="text-gray-500">Total Friends</p>
       </div>
       <div className="text-center space-y-3 p-5 rounded-xl shadow-md">
-        <h1 className="text-2xl md:text-3xl font-semibold">03</h1>
+        <h1 className="text-2xl md:text-3xl font-semibold">{onTrackCount.toString().padStart(2, '0')}</h1>
         <p className="text-gray-500">On Track</p>
       </div>
       <div className="text-center space-y-3 p-5 rounded-xl shadow-md">
-        <h1 className="text-2xl md:text-3xl font-semibold">06</h1>
+        <h1 className="text-2xl md:text-3xl font-semibold">{needAttentionCount.toString().padStart(2, '0')}</h1>
         <p className="text-gray-500">Need Attention</p>
       </div>
       <div className="text-center space-y-3 p-5 rounded-xl shadow-md">
